272a67c544a23260593ab60dc7df134bfc197d44
Nikhil Dhawan <nikhildhawan@nikhils-m1-mbp.myfiosgateway.com>
2023-09-03 14:21:55 -0400

    add support for posting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,20 @@ import {
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+  const ctx = api.useContext();
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -29,7 +38,16 @@ const CreatePostWizard = () => {
       <input
         placeholder="What's on your mind?!"
         className="grow bg-transparent"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      {isPosting ? (
+        <LoadingSpinner />
+      ) : (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
     </div>
   );
 };
@@ -69,7 +87,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {[...data, ...data]?.map((fullPost) => (
+      {data.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
